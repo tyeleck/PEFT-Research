5c0c9ede20718526961638ab2502beb8a9bd2c9e
tyeleck <tye.leckinger@gmail.com>
2023-06-06 20:17:09 -0400

    Update PEFT.py

--- PEFT.py
+++ PEFT.py
@@ -38,6 +38,22 @@ def printdf(data, nrow = 10):
 
 
 #%% Preprocessing Methods
+
+batch_size = 512* 2
+d_train = Sentiment140_Dataset(rel_path + "/data/train_data.csv")
+
+
+
+
+#d_test = Sentiment140_Dataset(rel_path + "/data/test_data.csv")
+train = torch.utils.data.DataLoader(d_train, batch_size = batch_size, shuffle = True)
+#test = torch.utils.data.DataLoader(d_test, batch_size = batch_size, shuffle = True)
+
+
+
+
+
+
 def one_hot_encode(data, dictionary):
     nrow = len(data)
     ncol = len(dictionary)
@@ -72,10 +88,9 @@ def generate_sequences(text_data,#training data
    # print(text_data)
     vocab = torchtext.vocab.build_vocab_from_iterator(tokens_gen, specials=["<unk>"])
     vocab.set_default_index(vocab["<unk>"])
-    vocab_size = len(vocab)
     
     vocab2text = lambda x: vocab(tok(x))
-    
+    vocab_size = len(vocab)
 
     tokens = torch.empty(size = (len(text_data),maxlen))
     np_text_data = np.array(text_data)
@@ -124,7 +139,7 @@ def generate_sequences(text_data,#training data
         else:
             indices.extend([vocab["<unk>"]] * pad_amount)
             
-     #   vocab_size_new = max(vocab_size,max(indices))
+        #vocab_size_new = max(vocab_size,max(indices))
       #  if(vocab_size_new != vocab_size):
       #      print(vocab_size_new)
      #       vocab_size = vocab_size_new
@@ -160,17 +175,17 @@ class Sentiment140_Dataset(Dataset):
 #%%
 
 
-batch_size = 512
-d_train = Sentiment140_Dataset(rel_path + "/data/train_data.csv")
-d_test = Sentiment140_Dataset(rel_path + "/data/test_data.csv")
-train = torch.utils.data.DataLoader(d_train, batch_size = batch_size, shuffle = True)
-test = torch.utils.data.DataLoader(d_test, batch_size = batch_size, shuffle = True)
 #%%
 
 d_train.x.size()
 
 #%%
-a = 10
+for i in range(len(d_train.x[:,])):
+    if(len(d_train.x[i,]) != 50):
+        print(i)
+
+
+
 
 #%%
 
@@ -182,7 +197,7 @@ hidden_dim = 64
 class LSTM_Network(nn.Module):
     def __init__(self, embedding_dim, hidden_dim, vocab_size, label_size):
         
-        print("LSTM Layer Vocab size: " + str(vocab_size))
+        #print("LSTM Layer Vocab size: " + str(vocab_size))
         
         super(LSTM_Network, self).__init__()
         self.hidden_dim = hidden_dim
@@ -191,66 +206,23 @@ class LSTM_Network(nn.Module):
 
         # The LSTM takes word embeddings as inputs, and outputs hidden states
         # with dimensionality hidden_dim.
-        self.l
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        stm = nn.LSTM(embedding_dim, hidden_dim)
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim)
 
         # The linear layer that maps from hidden state space to label space
-        self.hidden2tag = nn.Linear(hidden_dim, 1)
+        self.hidden2tag = nn.Linear(hidden_dim,1)
     
     def forward(self, sentence):
         
-       # print(sentence)
+        print(len(sentence))
        
        
-       
-        embeds = self.word_embeddings(sentence)
+        #print(type(sentence.long()))
+        embeds = self.word_embeddings(sentence.long())
         #embeds = torch.transpose(embeds,0,1)
         
         
         #print(embeds.reshape(-1,embedding_dim).size())
-        
-        embeds = embeds.view(len(sentence), -1, embedding_dim)
+        embeds = embeds.view(batch_size, -1, embedding_dim)
         
         lstm_out, _ = self.lstm(embeds)
         lstm_out = lstm_out[:, -1, :] 
@@ -262,16 +234,84 @@ class LSTM_Network(nn.Module):
         
         
         tag_space = self.hidden2tag(lstm_out)
-        tag_scores = nn.functional.log_softmax(tag_space, dim=2)
+        tag_scores = nn.functional.log_softmax(tag_space)
         return tag_scores
         
 model = LSTM_Network(embedding_dim, hidden_dim, d_train.tokenizer.vocab_size, 2)
-loss_function = nn.BCELoss()
+loss_fn = nn.BCELoss()
 optimizer = torch.optim.RMSprop(model.parameters(),lr = 0.01)
 
 
+#loss_fn = nn.BCELoss()  # binary cross entropy
+#optimizer = optim.Adam(model.parameters(), lr=0.001)
+ 
+n_epochs = 5    # number of epochs to run
+#batch_size = 10  # size of each batch
+batches_per_epoch = len(d_train.x) // batch_size
+ 
+
+
+model.to(device)
+
+for epoch in range(n_epochs):
+    print("Epoch: " + str(epoch + 1))
+    i = 0
+    for xbatch, ybatch in train:
+        #start = i * batch_size
+        # take a batch
+        #Xbatch = i
+        #ybatch = train.y[start:start+batch_size]
+        
+        
+        #ybatch = torch.from_numpy(ybatch)
+        
+        # forward pass
+        ybatch = ybatch.float()
+        ybatch = ybatch.unsqueeze(1)
+    
+        batch_size = len(ybatch)
+    
+    
+        Xbatch = Xbatch.to(device)
+        ybatch = ybatch.to(device)
+        y_pred = model(Xbatch)
+        
+        
+        loss = loss_fn(y_pred, ybatch)
+        # backward pass
+        optimizer.zero_grad()
+        loss.backward()
+        # update weights
+        optimizer.step()
+        
+        
+        predictions = y_pred.argmax(1)
+        Accuracy = torch.mean((predictions == ybatch).float())
+        
+        
+        i += 1
+        print(str(i) + "/" + str(batches_per_epoch) + " | " + str(Accuracy))
+        
+ 
+# evaluate trained model with test set
+#with torch.no_grad():
+ #   break
+    #y_pred = model(X)
+#accuracy = (y_pred.round() == y).float().mean()
+#print("Accuracy {:.2f}".format(accuracy * 100))
+
+
+
+
+
+
+#%%%
+
 
 def train_one_epoch(epoch_index, tb_writer):
+   # global inputs
+    #global labels
+    
     running_loss = 0.
     last_loss = 0.
 
@@ -388,17 +428,65 @@ model.print_trainable_parameters()
 
 
 #%%%
+
 maxlen = 50
 embedding_dim = 64
 hidden_dim = 64
 
+class LSTM_Network(nn.Module):
+    def __init__(self, embedding_dim, hidden_dim, vocab_size, label_size):
+        
+        print("LSTM Layer Vocab size: " + str(vocab_size))
+        
+        super(LSTM_Network, self).__init__()
+        self.hidden_dim = hidden_dim
 
+        self.word_embeddings = nn.Embedding(vocab_size, embedding_dim)
+
+        # The LSTM takes word embeddings as inputs, and outputs hidden states
+        # with dimensionality hidden_dim.
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim)
+
+        # The linear layer that maps from hidden state space to label space
+        self.hidden2tag = nn.Linear(hidden_dim, 1)
+    
+    def forward(self, sentence):
+        
+       # print(sentence)
+       
+       
+       
+        embeds = self.word_embeddings(sentence)
+        #embeds = torch.transpose(embeds,0,1)
+        
+        
+        #print(embeds.reshape(-1,embedding_dim).size())
+        
+        embeds = embeds.view(len(sentence), -1, embedding_dim)
+        
+        lstm_out, _ = self.lstm(embeds)
+        lstm_out = lstm_out[:, -1, :] 
+
+        
+        #print(lstm_out.size())
+        
+        
+        
+        
+        tag_space = self.hidden2tag(lstm_out)
+        tag_scores = nn.functional.log_softmax(tag_space)
+        return tag_scores
+        
 #model = LSTM_Network(embedding_dim, hidden_dim, d_train.tokenizer.vocab_size, 2)
 loss_function = nn.BCELoss()
 optimizer = torch.optim.RMSprop(model.parameters(),lr = 0.01)
 
 
+
 def train_one_epoch(epoch_index, tb_writer):
+   # global inputs
+    #global labels
+    
     running_loss = 0.
     last_loss = 0.
 
@@ -495,7 +583,6 @@ for epoch in range(EPOCHS):
     # Track best performance, and save the model's state
 
     epoch_number += 1
-
 
 
 
